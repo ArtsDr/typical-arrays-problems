c3296c189ce515176677037c167165d0f1e58c90
Artem Drozd <artsdrd@gmail.com>
2021-08-19 16:09:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+    for (let key in array) {
+        let min = array[0]
+
+        for (let i = 0; i < array.length; i++) {
+
+            if (array[i] < min) {
+                min = array[i]
+            }
+        }
+        return min
+    }
+    return 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+    for (let key in array) {
+        let max = array[0]
+
+        for (let i = 0; i < array.length; i++) {
+
+            if (array[i] > max) {
+                max = array[i]
+            }
+        }
+        return max
+    }
+    return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    for (let key in array) {
+        let sum = 0
+
+        for (let i = 0; i < array.length; i++) {
+
+            sum += array[i]
+        }
+        return sum / array.length
+    }
+    return 0
 }
